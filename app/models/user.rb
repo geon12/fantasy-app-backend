582d743f25d1d1ac682f3e69f720000f46afd81c
Cameron <camcreel@gmail.com>
2021-09-30 14:23:37 -0400

    Configure for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
     has_many :commissioners
     has_many :leagues, through: :commissioners
     has_many :fantasy_teams
