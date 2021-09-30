438467919c0e0fea27d938edd23810ffbc43f95b
Cameron <camcreel@gmail.com>
2021-09-29 20:02:33 -0400

    add users routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def show
+        @current_user = find_user
+        render json: @current_user
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:username,:email)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :leagues
+
+  post "/signup",to: "users#create"
+  get "/profile",to: "users#show"
+  patch "/profile", to: "users#update"
 end
