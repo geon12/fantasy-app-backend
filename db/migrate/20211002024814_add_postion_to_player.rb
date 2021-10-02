8b089b15b33d0f993a2588e79b39130603fd7f8b
Cameron <camcreel@gmail.com>
2021-10-01 22:53:57 -0400

    add position to player table

---
+++ db/migrate/20211002024814_add_postion_to_player.rb
@@ -0,0 +1,5 @@
+class AddPostionToPlayer < ActiveRecord::Migration[6.1]
+  def change
+    add_column :players, :position, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,30 +16,28 @@ puts("Seeding")
 TeamPlayer.destroy_all
 FreeAgent.destroy_all
 FantasyTeam.destroy_all
-PlayerPosition.destroy_all
 Commissioner.destroy_all
-Position.destroy_all
 Player.destroy_all
 League.destroy_all
 User.destroy_all
 #Create Player
 for player in player_data
-    Player.create({name:player[0], nba_team: player[3], active:true, fppg:player[5]})
+    Player.create({name:player[0], nba_team: player[3], active:true, fppg:player[5],position:player_data[1]})
 end
 
-#Create Posiitons
-Position.create(name: "Shooting Guard",shorthand:"SG")
-Position.create(name:"Center",shorthand:"C")
-Position.create(name:"Forward",shorthand:"F")
-Position.create(name:"Point Guard",shorthand:"PG")
-Position.create(name:"Small Forward",shorthand:"SF")
-Position.create(name:"Guard",shorthand:"G")
-Position.create(name:"Power Forward",shorthand:"PF")
+# #Create Posiitons
+# Position.create(name: "Shooting Guard",shorthand:"SG")
+# Position.create(name:"Center",shorthand:"C")
+# Position.create(name:"Forward",shorthand:"F")
+# Position.create(name:"Point Guard",shorthand:"PG")
+# Position.create(name:"Small Forward",shorthand:"SF")
+# Position.create(name:"Guard",shorthand:"G")
+# Position.create(name:"Power Forward",shorthand:"PF")
 
-#Create Player Posiitons
-for player in player_data
-    PlayerPosition.create(player:Player.find_by(name:player[0]),position:Position.find_by(shorthand:player_data[1]))
-end
+# #Create Player Posiitons
+# for player in player_data
+#     PlayerPosition.create(player:Player.find_by(name:player[0]),position:Position.find_by(shorthand:player_data[1]))
+# end
 
 #Create Users
 10.times do
