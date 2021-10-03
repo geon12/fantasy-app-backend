8a423b1c1463e8b564349a737145a27170e7f851
Cameron <camcreel@gmail.com>
2021-10-03 12:43:54 -0400

    add nested route for free agents

--- app/controllers/free_agents_controller.rb
+++ app/controllers/free_agents_controller.rb
@@ -1,2 +1,11 @@
 class FreeAgentsController < ApplicationController
+    def index
+        if params[:league_id]
+          league = League.find(params[:league_id])
+          free_agents = league.free_agents
+        else
+          free_agents = FreeAgent.all
+        end
+        render json: free_agents
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :leagues
+  resources :leagues do
+    resources :free_agents, only [:show]
+  end
   resources :fantasy_teams
   resources :team_players, only: [:create,:destroy]
-  get "/free_agents", to: "free_agents#show"
+  #get "/free_agents", to: "free_agents#index"
 
   post "/signup", to: "users#create"
   post '/login', to: 'auth#create'
