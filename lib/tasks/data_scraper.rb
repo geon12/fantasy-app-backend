7aa904aba1b7cf71313326cb4e6239a94328d912
Cameron <camcreel@gmail.com>
2021-10-09 13:13:43 -0400

    add parse function to data scraper

--- lib/tasks/data_scraper.rb
+++ lib/tasks/data_scraper.rb
@@ -2,9 +2,14 @@ require 'kimurai'
 
 class DataScraper < Kimurai::Base
     @name= 'nba_scraper'
-    @start_urls = ["https://www.nba.com/stats/players/traditional/?sort=PTS&dir=-1"]
+    @start_urls = ["https://www.basketball-reference.com/leagues/NBA_2021_per_game.html"]
     @engine = :mechanize
 
     def parse(response, url:, data: {})
+     table = response.xpath("//div[@id='per_game_stats']/div/a")
+    end
+
+    def parse_box_score(response, url:, data: {})
+
     end
 end
